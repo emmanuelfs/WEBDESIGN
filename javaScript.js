861e7707b519538aa25b0c1291af85eb3e4982d2
emmanuelfs <efsantinoli@gmail.com>
2021-09-27 17:57:01 -0300

    Base

---
+++ javaScript.js
@@ -0,0 +1,6 @@
+test = document.querySelector(".important-title-h1");
+
+//test.textContent = "New Test";
+//test.classList.add("changingColor");
+test.className += " " + "changingColor";
+document.write(test);
